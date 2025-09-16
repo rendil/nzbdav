72668d02b5b0e829e3e581da8aad159ce0da2e22
Matt Alexander <matt.alexander@apollographql.com>
2025-09-15 21:02:33 -0700

    Let's try ubuntu

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # Use env vars or default to 1000
 PUID=${PUID:-1000}
@@ -6,13 +6,13 @@ PGID=${PGID:-1000}
 
 # Create group if it doesn't exist
 if ! getent group appgroup >/dev/null; then
-    addgroup -g "$PGID" appgroup
+    groupadd -g "$PGID" appgroup
 fi
 
 # Create user if it doesn't exist
 if ! id appuser >/dev/null 2>&1; then
-    adduser -D -H -u "$PUID" -G appgroup appuser
+    useradd -M -u "$PUID" -g appgroup appuser
 fi
 
 # Switch to new user and run app
-exec su-exec appuser ./NzbWebDAV
+exec gosu appuser ./NzbWebDAV
